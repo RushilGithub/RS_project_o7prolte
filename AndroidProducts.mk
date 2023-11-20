6565542e79a7c0989d73aa261896ea06cfddd5d1
Rushil <138013143+RushilGithub@users.noreply.github.com>
2023-11-20 22:09:32 +0530

    update of lineage-18.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := device/samsung/o7prolte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_o7prolte.mk \
-	$(LOCAL_DIR)/rr_o7prolte.mk \
 	$(LOCAL_DIR)/lineage_o7prolte.mk
 
 COMMON_LUNCH_CHOICES := \
